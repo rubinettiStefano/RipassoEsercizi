bf45c296ca74ec9a80ee43fdbaab4839de875c0d
StefanoR <rubinetti.stefano@gmail.com>
2025-03-28 14:20:12 +0100

    3

---
+++ src/main/java/Esercizio3.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Esercizio3
+{
+	public static void main(String[] args)
+	{
+		List<Mobile> tutti = new ArrayList<>();
+		tutti.add(new Mobile("Armadio", "bianco", 75.0));
+		tutti.add(new Mobile("Scrivania", "nero", 40.5));
+		tutti.add(new Mobile("Comodino", "marrone", 12.0));
+		tutti.add(new Mobile("Libreria", "bianco", 60.0));
+		tutti.add(new Mobile("Cassettiera", "nero", 30.0));
+		tutti.add(new Mobile("Tavolo", "marrone", 50.0));
+		tutti.add(new Mobile("Sedia", "nero", 8.5));
+		tutti.add(new Mobile("Credenza", "bianco", 55.0));
+		tutti.add(new Mobile("Mobile TV", "marrone", 33.3));
+		tutti.add(new Mobile("Scarpiera", "bianco", 20.0));
+
+		//1) Stampare tutti i mobili marroni
+		//2) Stampare tutti i mobili marroni che pesano più di 40
+		//3) stampare una lista con solo i nomi dei mobili
+		//4) stampare solo i colori una volta sola (bianco,nero,marrone)
+		//5) stampare il peso medio dei mobili
+		//6) stampare i 5 mobili più pesanti
+		//7) Stampare solo la Sedia, trovandola grazie al nome
+	}
+}
---
+++ src/main/java/Mobile.java
@@ -0,0 +1,42 @@
+public class Mobile
+{
+	private String nome,colore;
+	private double peso;
+
+	public Mobile(String nome, String colore, double peso)
+	{
+		this.nome = nome;
+		this.colore = colore;
+		this.peso = peso;
+	}
+
+	public String getNome()
+	{
+		return nome;
+	}
+
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+
+	public String getColore()
+	{
+		return colore;
+	}
+
+	public void setColore(String colore)
+	{
+		this.colore = colore;
+	}
+
+	public double getPeso()
+	{
+		return peso;
+	}
+
+	public void setPeso(double peso)
+	{
+		this.peso = peso;
+	}
+}
